e96834641aa81d9a3c9491b21d1a7e7a475f5e3f
Muneeb wali khan <126275491+MuneebWaliKhan09@users.noreply.github.com>
2024-05-16 18:20:56 +0500

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,6 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://school-managment-system-xlqq-frontend.vercel.app');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 
