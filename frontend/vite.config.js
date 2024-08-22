c32ce85763ea90716195fae8c704d2a086c41e3b
MuneebWaliKhan09 <muneebwali809@gmail.com>
2024-08-22 22:52:17 +0500

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api/v1': {
-        // target: 'http://localhost:3000', // Change this to match your backend server's URL
-        target: 'https://school-managment-system-pi.vercel.app', // Change this to match your backend server's URL
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     '/api/v1': {
+  //       // target: 'http://localhost:3000', // Change this to match your backend server's URL
+  //       target: 'https://school-managment-system-pi.vercel.app', // Change this to match your backend server's URL
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   },
+  // },
 })
